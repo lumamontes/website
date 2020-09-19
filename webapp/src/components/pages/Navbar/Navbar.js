43ae554a07667248ab75af916cc4eb6b89a38450
Luma Montes <lumagoesmontes@gmail.com>
2020-09-18 22:58:17 -0300

    iniciado componente da header

---
+++ webapp/src/components/pages/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { MdFingerprint } from 'react-icons/md'
+import { FaBars, FaTimes } from 'react-icons/fa'
+
+function Navbar() {
+    return (
+        <>
+            <div className="navbar">
+                <div className="navbar-container container">
+                    <Link to='/' className="navbar-logo">
+                       <MdFingerprint className='navbar-icon' />
+                        LAVAISH
+                     </Link>
+                     <div className="menu-icon">
+                         
+                     </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+export default Navbar;
