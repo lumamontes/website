4890edf3fef99821abdb01905fa23c70bb620897
Luma Montes <lumagoesmontes@gmail.com>
2020-09-19 00:30:09 -0300

    criado logica de personalizacao de botao

---
+++ webapp/src/Button.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Button.css'
+const STYLES = ['btn--primary', 'btn--outline']
+
+
+const SIZES = [ 'btn--medium', 'btn--large', 'btn--mobile', 'btn--wide']
+
+const COLOR = ['primary', 'secondary']
+
+export const Button = ({children, type, onClick, buttonStyle, buttonSize, buttonColor}) => {
+    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES [0]
+    const checkButtonSize = STYLES.includes(buttonSize) ? buttonSize : SIZES [0]
+    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES [0]
+    
+    return (
+<button className={`btn ${checkButtonStyle} ${checkButtonSize} ${checkButtonColor}`} onClick={onClick} type={type}>{children}</button>
+)}
